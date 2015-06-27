42599bbc59655abbfbfb65f3507a25f0282b4858
Viktor Pekar <vikpek@gmail.com>
2015-06-27 13:34:29 +0200

    basic health functionality

---
+++ BerlinMiniJamJune2015/Assets/Scripts/UnitController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitController : MonoBehaviour {
+
+	[SerializeField]
+	int health = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessHealth();
+	}
+
+
+	void ProcessHealth(){
+		if(health < 1)
+		{
+			ProcessDeath();
+		}
+	}
+
+	void ProcessDeath(){
+		// TODO tbd
+	}
+	
+	void OnCollisionEnter(Collision col){
+		Debug.Log("test");
+	}
+}
