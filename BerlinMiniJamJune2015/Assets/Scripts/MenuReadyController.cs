46c394f445abb885ae6c6aec4555480283b3318c
Viktor Pekar <vikpek@gmail.com>
2015-06-27 17:52:44 +0200

    - menus, game over etc.

--- BerlinMiniJamJune2015/Assets/Scripts/MenuReadyController.cs
+++ BerlinMiniJamJune2015/Assets/Scripts/MenuReadyController.cs
@@ -24,23 +24,27 @@ public class MenuReadyController : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.W) && !playerA){
 			goPlayerA.GetComponent<Text>().color = Color.green;
 			goPlayerA.GetComponent<Text>().text = "Yeah!";
+			goPlayerA.GetComponent<Text>().transform.Rotate(new Vector3(0,0,180));
 			playerA = true;	
 		}
 		if (Input.GetKeyDown(KeyCode.A) && !playerB){
 			goPlayerB.GetComponent<Text>().color = Color.green;
 			goPlayerB.GetComponent<Text>().text = "Let's go!";
+			goPlayerB.GetComponent<Text>().transform.Rotate(new Vector3(0,0,180));
 			playerB = true;
 		}
 
 		if (Input.GetKeyDown(KeyCode.S) && !playerC){
 			goPlayerC.GetComponent<Text>().color = Color.green;
 			goPlayerC.GetComponent<Text>().text = "What are you waiting for?";
+			goPlayerC.GetComponent<Text>().transform.Rotate(new Vector3(0,0,180));
 			playerC = true;
 		}
 
 		if (Input.GetKeyDown(KeyCode.D) && !playerD){
 			goPlayerD.GetComponent<Text>().color = Color.green;
 			goPlayerD.GetComponent<Text>().text = "Let's roll!";
+			goPlayerD.GetComponent<Text>().transform.Rotate(new Vector3(0,0,180));
 			playerD = true;
 		}
 
